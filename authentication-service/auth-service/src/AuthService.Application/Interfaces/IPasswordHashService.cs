7e1969a9006c1d611b8a18dc82aad09e92439249
cnavarro-2024170 <cnavarro-2024170@kinal.edu.gt>
2026-02-20 15:55:38 -0600

    feat:Se agrego el login del gestor de opiniones con .NET

---
+++ authentication-service/auth-service/src/AuthService.Application/Interfaces/IPasswordHashService.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace AuthService.Application.Interfaces;
+
+public interface IPasswordHashService
+{
+    string HashPassword(string password);
+    bool VerifyPassword(string password, string hashedPassword);
+}
---
+++ authentication-service/auth-service/src/AuthService.Domain/Enums/UserRole.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace AuthService.Domain.Enums;
+
+public enum UserRole
+{
+    USER_ROLE,
+    ADMIN_ROLE
+}
